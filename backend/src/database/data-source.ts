d20e0091161d1fefac21ec079e3b79c44e88f54e
Emerson Souza <emersonsouza8@hotmail.com>
2026-01-22 15:26:45 -0300

    ajuste: conexao banco railway e seed

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -7,12 +7,12 @@ import { Appointment } from "./entities/Appointment";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 3306,
-    username: process.env.MYSQLUSER || "root",     // Mudamos aqui
-    password: process.env.MYSQLPASSWORD || "root", // Mudamos aqui
-    database: process.env.MYSQLDATABASE || "med_schedule", // Mudamos aqui
-    synchronize: true, // No primeiro deploy, mude para true para ele criar as tabelas sozinho
+    host: process.env.MYSQLHOST || "localhost",
+    port: Number(process.env.MYSQLPORT) || 3306,
+    username: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "root",
+    database: process.env.MYSQLDATABASE || "med_schedule",
+    synchronize: true, 
     logging: true,
     entities: [User, Doctor, Exam, Appointment],
     migrations: ["./src/database/migrations/*.ts"],
